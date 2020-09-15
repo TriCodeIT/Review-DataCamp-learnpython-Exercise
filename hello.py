7caa5641d723d4a0182b4ef75b3695bef7d1bb03
TriCodeIT <bayu165kusuma@gmail.com>
2020-09-15 08:09:18 +0700

    Review Conditions, Loops, Functions and others

---
+++ hello.py
@@ -0,0 +1,5 @@
+print('Hello World')
+
+print("This line will be printed")
+
+print("I can use Python \nthat`s it")
---
+++ list.py
@@ -0,0 +1,20 @@
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+
+numbers.append(7)
+numbers.append(8)
+numbers.append(9)
+
+strings.append('That is')
+strings.append('easy')
+
+# write your code here
+second_name = names[2]
+
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
